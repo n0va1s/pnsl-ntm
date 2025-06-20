961ae71b6611512f8d2a07cbb02a5a45e2f076d0
Joao Paulo Novais <jp.trabalho@gmail.com>
2025-06-20 13:04:20 -0300

    Correcao da model de evento ao gerar registros com a factory

--- app/app/Models/Evento.php
+++ app/app/Models/Evento.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model
 {
+    use HasFactory;
+    
     protected $table = 'evento';
     protected $primaryKey = 'idt_evento';
 
