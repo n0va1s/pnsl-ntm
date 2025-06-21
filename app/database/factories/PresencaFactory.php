43d8e0d0e33e25172799a0c57c172c4c32563a2f
Gabriel <oliveiracgabriel052@gmail.com>
2025-06-21 10:19:21 -0300

    Criação da Model, Factory e Seeder da Presenca

---
+++ app/database/factories/PresencaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PresencaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ind_par' => Participante::inRandomOrder()->first()->idt_participante ?? Participante::factory(),
+            'dat_presenca' => $this->faker->dateTimeBetween('-1 month', 'now')->format('d/m/Y'),
+            'inc_presenca' => $this->faker->boolean(80), // 80% chance de presença
+        ];
+    }
+}
