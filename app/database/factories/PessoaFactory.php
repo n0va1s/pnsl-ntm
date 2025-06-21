1e633fb78ca1abff7e5f3d3c579a96f35b0f772b
Gabriel <oliveiracgabriel052@gmail.com>
2025-06-21 10:19:01 -0300

    Atualização da Model e Factory da Pessoa

--- app/database/factories/PessoaFactory.php
+++ app/database/factories/PessoaFactory.php
@@ -13,11 +13,15 @@ class PessoaFactory extends Factory
     {
         return [
             'nom_pessoa' => $this->faker->name(),
-            'des_telefone' => $this->faker->phoneNumber(),
+            'nom_apelido' => $this->faker->lastName(),
+            'tip_genero' => $this->faker->randomElement(['M', 'F']), // Masculino, Feminino
+            'tel_pessoa' => $this->faker->phoneNumber(),
+            'eml_pessoa' => $this->faker->unique()->safeEmail(),
             'des_endereco' => $this->faker->address(),
             'dat_nascimento' => $this->faker->date('Y-m-d', '-20 years'),
             'tam_camiseta' => $this->faker->randomElement(['P', 'M', 'G', 'GG']),
             'ind_toca_instrumento' => $this->faker->boolean(),
+            'ind_consentimento' => $this->faker->randomElement(['sim', 'não']),
         ];
     }
 }
