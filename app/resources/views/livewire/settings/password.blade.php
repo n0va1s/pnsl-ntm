3976af8a4fc24d696910980c846e093827e56886
Gabriel <oliveiracgabriel052@gmail.com>
2025-07-21 11:45:40 -0300

    Tradução das mensagens de erro

--- app/resources/views/livewire/settings/password.blade.php
+++ app/resources/views/livewire/settings/password.blade.php
@@ -40,25 +40,25 @@ new class extends Component {
 <section class="w-full">
     @include('partials.settings-heading')
 
-    <x-settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
+    <x-settings.layout :heading="__('Update password')" subheading="Garanta que sua conta esteja usando uma senha longa e aleatória para manter a segurança">
         <form wire:submit="updatePassword" class="mt-6 space-y-6">
             <flux:input
                 wire:model="current_password"
-                :label="__('Current password')"
+                :label="__('Senha atual')"
                 type="password"
                 required
                 autocomplete="current-password"
             />
             <flux:input
                 wire:model="password"
-                :label="__('New password')"
+                :label="__('Nova senha')"
                 type="password"
                 required
                 autocomplete="new-password"
             />
             <flux:input
                 wire:model="password_confirmation"
-                :label="__('Confirm Password')"
+                :label="__('Confirmar senha')"
                 type="password"
                 required
                 autocomplete="new-password"
@@ -66,11 +66,11 @@ new class extends Component {
 
             <div class="flex items-center gap-4">
                 <div class="flex items-center justify-end">
-                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Save') }}</flux:button>
+                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Salvar') }}</flux:button>
                 </div>
 
                 <x-action-message class="me-3" on="password-updated">
-                    {{ __('Saved.') }}
+                    {{ __('Salvo.') }}
                 </x-action-message>
             </div>
         </form>
