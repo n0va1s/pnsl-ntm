4c898398ba19ed1d4b928a39520ee99782c378f9
Gabriel <oliveiracgabriel052@gmail.com>
2025-07-29 18:38:38 -0300

    Alteração da Logo

--- app/resources/views/components/app-logo-icon.blade.php
+++ app/resources/views/components/app-logo-icon.blade.php
@@ -1,4 +1,2 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" {{ $attributes }}>
-    <path fill="currentColor"
-        d="M25 2C12.31 2 2 12.31 2 25s10.31 23 23 23 23-10.31 23-23S37.69 2 25 2zm0 4c10.47 0 19 8.53 19 19s-8.53 19-19 19-19-8.53-19-19 8.53-19 19-19zm0 5v14h-7v4h7v14h4V32h7v-4h-7V11h-4z" />
-</svg>
+<img src="https://i.imgur.com/pKzoj0J.png" {{ $attributes->merge(['class' => 'block dark:hidden']) }} alt="Logo Claro">
+<img src="https://i.imgur.com/c74GSoI.png" {{ $attributes->merge(['class' => 'hidden dark:block']) }} alt="Logo Escuro">
